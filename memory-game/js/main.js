c0e1b7297f5162ab461ec790dc16ec77d647e4ac
User <user@MacBook-Pro-94.local>
2020-03-12 16:27:48 -0700

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+//main.js
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardFour);
